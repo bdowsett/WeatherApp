19b1d97cb9b06d24559f5ca1f41f5089eb1ed1df
bdowsett <bendowsett@gmail.com>
2023-07-11 17:19:14 +0100

    add constants object

---
+++ app/src/main/java/com/example/weatherapp/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.common
+
+object Constants {
+    const val API_KEY = ""
+}
--- app/src/main/java/com/example/weatherapp/data/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/data/WeatherRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp.data
 
+import com.example.weatherapp.common.Constants
 import com.example.weatherapp.data.model.WeatherData
 import com.example.weatherapp.repository.WeatherRepository
 import retrofit2.Response
@@ -7,7 +8,7 @@ import javax.inject.Inject
 
 class WeatherRepositoryImpl @Inject constructor(private val api: WeatherApi) : WeatherRepository{
     override suspend fun getWeather(): Response<WeatherData> {
-        val apiKey = "0f485acabb154aeab8a172221230607"
+        val apiKey = Constants.API_KEY
         val location = "London"
         val days = 7
         return api.getWeather(apiKey = apiKey, forecastDays = days, location = location)
